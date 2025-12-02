384de53de1a05d1dfd9932d8b22e8934effc7b6f
yjs2673 <yjs2673@naver.com>
2025-12-02 06:16:53 +0000

    VM Initialization 추가

--- src/threads/init.c
+++ src/threads/init.c
@@ -37,6 +37,9 @@
 #include "filesys/filesys.h"
 #include "filesys/fsutil.h"
 #endif
+#include "vm/frame.h"
+#include "vm/page.h"
+#include "vm/swap.h"
 
 /* Page directory with kernel mappings only. */
 uint32_t *init_page_dir;
@@ -119,6 +122,11 @@ main (void)
   thread_start ();
   serial_init_queue ();
   timer_calibrate ();
+
+  /* VM Initialization */
+  frame_init ();
+  vm_init ();
+  swap_init ();
 
 #ifdef FILESYS
   /* Initialize file system. */
