065508415ccba851ea4909eb0d5d7cd829422e81
yjs2673 <yjs2673@naver.com>
2025-12-07 05:17:45 +0000

    page 헤더 완료

--- src/vm/page.h
+++ src/vm/page.h
@@ -6,35 +6,41 @@
 #include "debug.h"
 #include "lib/kernel/hash.h"
 #include "threads/synch.h"
-#define VM_BIN 0
-#define VM_FILE 1
-#define VM_ANON 2
 
-typedef enum { BINARY, ANON, SWAPPED } pt_type;
+#define VM_BIN  0x00
+#define VM_FILE 0x01
+#define VM_ANON 0x02
+
+typedef enum { 
+    BINARY, 
+    ANON, 
+    SWAPPED 
+} pt_type;
 
 struct pt_entry 
 {
-  void *vaddr;                  
-  pt_type type;                 
-  
+  struct file *file;            
   size_t offset;                
   size_t read_bytes;            
   size_t zero_bytes;            
+
+  /* 가상 주소 및 상태 정보 */
+  void *vaddr;                  
+  size_t swap_slot;             
   
-  bool writable;                
+  /* 상태 플래그 */
   bool is_loaded;               
+  bool writable;                
+  pt_type type;                 
 
   struct hash_elem elem;        
-
-  struct file *file;            
-  size_t swap_slot;             
 };
 
 void pt_init (struct hash *pt);
-void pt_destroy (struct hash *pt);
-bool pt_insert_entry (struct hash *pt, struct pt_entry *pte);
-bool pt_delete_entry (struct hash *pt, struct pt_entry *pte);
 struct pt_entry *pt_find_entry (void *vaddr);
 struct pt_entry *pt_lookup_entry (struct hash *pt, void *vaddr);
+bool pt_insert_entry (struct hash *pt, struct pt_entry *pte);
+bool pt_delete_entry (struct hash *pt, struct pt_entry *pte);
+void pt_destroy (struct hash *pt);
 
 #endif
