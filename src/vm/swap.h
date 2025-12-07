5cc05b64bc0d3c52815c2ab5b4de83d6443b79da
yjs2673 <yjs2673@naver.com>
2025-12-07 05:55:51 +0000

    swap 헤더 완료

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -5,20 +5,18 @@
 #include "threads/vaddr.h"
 #include "devices/block.h"
 #include "threads/synch.h"
-#include "vm/frame.h"
-#include "vm/page.h"
 #include "userprog/pagedir.h"
 #include "threads/thread.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
+#include "vm/frame.h"
+#include "vm/page.h"
 
-#define BLOCK_MAX 8    
+#define BLOCK_MAX 0x08 
 
 void swap_init(void);
-void swap_in(size_t idx, void *paddr);
 size_t swap_out(void *paddr);
+void swap_in(size_t idx, void *paddr);
 void handle_block(size_t index, void* kaddr, bool r_w);
-
-
 
 #endif
