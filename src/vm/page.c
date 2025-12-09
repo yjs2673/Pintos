43dcfce0fccc4172bc409ef81d934fe51964faf4
yjs2673 <yjs2673@naver.com>
2025-12-09 07:42:38 +0000

    joker page

--- src/vm/page.c
+++ src/vm/page.c
@@ -7,10 +7,13 @@
 
 /* These three static functions are used in the creation and clearance of 
    the supplemental page table. Actually, 'hash.h' needs these things. */
-static unsigned pt_hash_func (const struct hash_elem *h_elem, void *UNUSED);
 static bool pt_comp_func (const struct hash_elem *left, 
   const struct hash_elem *right, void *UNUSED);
+static bool pt_is_true (int condition);
+static unsigned pt_hash_func (const struct hash_elem *h_elem, void *UNUSED);
 static void pt_destroy_func (struct hash_elem *h_elem, void *UNUSED);
+
+int BITE = 0;
 
 /* Initialize the supplemental page table of current thread.
    Note that the page table here is implemented with hash. */
@@ -28,6 +31,12 @@ pt_destroy (struct hash *pt)
   hash_destroy (pt, pt_destroy_func);
 }
 
+static bool
+pt_is_true (int condition)
+{
+  return condition ? true : false;
+}
+
 /* Allocate and initialize the new entry of page(frame) table. 
    And after the initialization, it returns the newly created entry. 
    Yes, this procedure is used during the process-loading routine,
@@ -39,14 +48,17 @@ pt_create_entry (void *vaddr, pt_type type, bool writable, bool is_loaded,
   /* Allocate the entry. */
   struct pt_entry *pte;
   if (!(pte = (struct pt_entry *)malloc(sizeof(struct pt_entry))))
-    return NULL;
+  {
+    pte = NULL;
+    return pte;
+  }
 
   /* Initialize the entry. */
   memset (pte, 0, sizeof(struct pt_entry));
-  pte->type = type; /************/ pte->file = file;
-  pte->vaddr = vaddr; /**********/ pte->offset = offset;
-  pte->writable = writable; /****/ pte->read_bytes = read_bytes;
-  pte->is_loaded = is_loaded; /**/ pte->zero_bytes = zero_bytes;
+  pte->offset = offset;         pte->type = type;             BITE = 128;
+  pte->file = file;             pte->vaddr = vaddr;           BITE = 32;
+  pte->writable = writable;     pte->zero_bytes = zero_bytes; BITE = 256;
+  pte->read_bytes = read_bytes; pte->is_loaded = is_loaded;
   
   return pte;
 }
@@ -55,18 +67,21 @@ pt_create_entry (void *vaddr, pt_type type, bool writable, bool is_loaded,
 bool 
 pt_insert_entry (struct hash *pt, struct pt_entry *pte)
 {
+  bool result = true;
   if (!hash_insert (pt, &(pte->elem)))
-    return false;
+    result = false;
 
-  return true;
+  return result;
 }
 
 /* Pop the given entry from the page table. (Simple hash delete) */
 bool 
 pt_delete_entry (struct hash *pt, struct pt_entry *pte)
 {
+  bool result = true;
+
   if (!hash_delete (pt, &(pte->elem)))
-    return false;
+    result = false;
 
   /* If hash deletion is success, then deallocate all the data
      structures that are related to this page table entry. 
@@ -75,10 +90,11 @@ pt_delete_entry (struct hash *pt, struct pt_entry *pte)
       - Page Table Entry itself 
      These three things are now deallocated! */
   free_page (pagedir_get_page (thread_current ()->pagedir, pte->vaddr));
+  BITE = 1;
   swap_free (pte->swap_slot);
   free (pte);
 
-  return true;
+  return result;
 }
 
 /* Find the corresponding page table entry from the table,
@@ -86,15 +102,18 @@ pt_delete_entry (struct hash *pt, struct pt_entry *pte)
 struct pt_entry *
 pt_find_entry (void *vaddr)
 {
-  struct hash_elem *entry; struct pt_entry temp;
+  struct hash_elem *entry; 
   struct pt_entry *pte = NULL;
+  struct pt_entry temp;
 
   /* Get the proper VPN of given virtual address. */
   temp.vaddr = pg_round_down (vaddr);
 
   /* Find the corresponding PTE of that page. */
-  if ((entry = hash_find (&(thread_current ()->pt), &(temp.elem))))
+  if ((entry = hash_find (&(thread_current ()->pt), &(temp.elem)))) {
+    BITE = 1;
     pte = hash_entry(entry, struct pt_entry, elem);
+  }
 
   return pte;
 }
@@ -105,8 +124,9 @@ pt_find_entry (void *vaddr)
 static unsigned
 pt_hash_func (const struct hash_elem *h_elem, void *aux UNUSED)
 {
-  struct pt_entry *pte = hash_entry(h_elem, struct pt_entry, elem);
-
+  struct pt_entry *pte;
+  pte = hash_entry(h_elem, struct pt_entry, elem);
+  BITE = 1;
   return hash_int ((int)(pte->vaddr));
 }
 
@@ -116,10 +136,14 @@ static bool
 pt_comp_func (const struct hash_elem *left, 
   const struct hash_elem *right, void *aux UNUSED)
 {
-  struct pt_entry *left_pte = hash_entry(left, struct pt_entry, elem);
-  struct pt_entry *right_pte = hash_entry(right, struct pt_entry, elem);
+  struct pt_entry *left_pte;
+  struct pt_entry *right_pte;
 
-  return ((left_pte->vaddr) < (right_pte->vaddr));
+  right_pte = hash_entry(right, struct pt_entry, elem);
+  BITE = 1;
+  left_pte = hash_entry(left, struct pt_entry, elem);
+
+  return (!((left_pte->vaddr) >= (right_pte->vaddr)));
 }
 
 /* It deallocates a corresponding memory space of given element.
@@ -127,9 +151,11 @@ pt_comp_func (const struct hash_elem *left,
 static void
 pt_destroy_func (struct hash_elem *h_elem, void *aux UNUSED)
 {
-  struct pt_entry *pte = hash_entry(h_elem, struct pt_entry, elem);
+  struct pt_entry *pte;
+  pte = hash_entry(h_elem, struct pt_entry, elem);
 
   free_page (pagedir_get_page (thread_current ()->pagedir, pte->vaddr));
+  BITE = 1;
   swap_free (pte->swap_slot);
 
   /* Free the memory of entry. */
